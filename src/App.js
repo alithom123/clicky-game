f8a27514e2967ebd0ae847b547a4b3674a47d4e0
Ali Thompson <alithom123@gmail.com>
2019-05-05 01:16:24 -0500

    add images to cards. create array of cards.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,64 @@ import './App.css';
 import Card from "./components/Card";
 
 function App() {
+  var imageId=1;
+  // var imageUrl="https://upload.wikimedia.org/wikipedia/commons/d/d1/Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg";
+
+  var imageUrl="/A.jpg";
+  var game = [
+      {
+        id: 1,
+        imageUrl: "/A.jpg",
+        clickCount: 0
+      },
+      {
+        id: 2,
+        imageUrl: "/b.jpg",
+        clickCount: 0
+      },
+      {
+        id: 3,
+        imageUrl: "/C.jpg",
+        clickCount: 0
+      },
+      {
+        id: 4,
+        imageUrl: "/D.jpg",
+        clickCount: 0
+      },
+      {
+        id: 5,
+        imageUrl: "/E.jpg",
+        clickCount: 0
+      },
+      {
+        id: 6,
+        imageUrl: "/F.jpg",
+        clickCount: 0
+      },
+      {
+        id: 7,
+        imageUrl: "/G.jpg",
+        clickCount: 0
+      },
+      {
+        id: 8,
+        imageUrl: "/H.jpg",
+        clickCount: 0
+      },
+      {
+        id: 9,
+        imageUrl: "/I.jpg",
+        clickCount: 0
+      },
+    ];
+
+    function shuffleCards(cardsArray) {
+      
+    };
+
+  
+
   return (
     <div className="App">
       {/* <header className="App-header">
@@ -20,11 +78,11 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card imageUrl={imageUrl} imageId={imageId} />
+      <Card imageUrl={imageUrl} imageId={imageId} />
+      <Card imageUrl={imageUrl} imageId={imageId} />
+      <Card imageUrl={imageUrl} imageId={imageId} />
+      <Card imageUrl={imageUrl} imageId={imageId} />
     </div>
   );
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,7 +5,7 @@ function Card(props) {
     return (
         <div>
         <div className="card" style={{width: "18rem"}}>
-        <img className="card-img-top" src="..." alt="Card image cap" />
+        <img className="card-img-top" src={props.imageUrl} alt="Card image cap" />
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
