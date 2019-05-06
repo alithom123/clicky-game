7dc9ba82cfd76b04475265fb78e6ea52cee73a73
Ali Thompson <alithom123@gmail.com>
2019-05-06 12:46:15 -0500

    Updates

---
+++ precache-manifest.1d16c54fe4e1452fe040d899a418ae6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45df122885b24f7143207a0b992b3f0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "cb2b73c368c67b118603",
+    "url": "/clicky-game/static/css/main.b1fba8dc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clicky-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cb2b73c368c67b118603",
+    "url": "/clicky-game/static/js/main.3cb8a82c.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
