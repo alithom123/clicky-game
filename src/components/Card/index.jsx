dd0b637cfc6a0ea7661f24d9c27e125322dee78b
Ali Thompson <alithom123@gmail.com>
2019-05-06 15:20:22 -0500

    Changed card to a class component and added a clickedOnce state.  Not sure whether i'll keep state in here or in the Game.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import "./style.css";
 
-function Card(props) {
-    return (
-        <a href="" class="custom-card">
-            <div>
-                <div className="click-item" >
-                    <img className="card-img-top" src={props.imageUrl} />
-                    <div className="card-body">
-                        {/* <h5 className="card-title">Card title</h5> */}
-                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
-                        {/* <a href="#" className="btn btn-primary" onClick={props.handleCardClick} id={props.id} clickCount={props.clickCount}>Click this card</a> */}
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clickedOnce: false,
+        };
+    }
+
+
+    render() {
+        return (
+            <a href="" class="custom-card">
+                <div>
+                    <div className="click-item" >
+                        <img className="card-img-top" src={this.props.imageUrl} />
+                        <div className="card-body">
+                            {/* <h5 className="card-title">Card title</h5> */}
+                            {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                            {/* <a href="#" className="btn btn-primary" onClick={props.handleCardClick} id={props.id} clickCount={props.clickCount}>Click this card</a> */}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </a>
-    );
+            </a>
+        );
+    }
 }
 
 export default Card;
