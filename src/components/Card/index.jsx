c7163a476e148aef610c1e84e995f76cbbdefe0e
Ali Thompson <alithom123@gmail.com>
2019-05-06 17:41:00 -0500

    moved onclick to div so acts on whole card and doesn't refresh like the <a> tag did.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -12,18 +12,21 @@ class Card extends React.Component {
 
     render() {
         return (
-            <a href="" class="custom-card">
-                <div>
-                    <div className="click-item" >
-                        <img className="card-img-top" src={this.props.imageUrl} />
-                        <div className="card-body">
-                            {/* <h5 className="card-title">Card title</h5> */}
-                            {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
-                            {/* <a href="#" className="btn btn-primary" onClick={props.handleCardClick} id={props.id} clickCount={props.clickCount}>Click this card</a> */}
-                        </div>
+            // <a href="" class="custom-card">
+            <div onClick={() => this.props.onClick()}>
+                <div className="click-item" >
+                    <img className="card-img-top" src={this.props.imageUrl} />
+                    <div className="card-body">
+                        <p>clicked = {this.state.clickedOnce ? "true" : "false"}</p>
+                        <p>id = {this.props.id}</p>
+
+                        {/* <h5 className="card-title">Card title</h5> */}
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        {/* <a href="#" className="btn btn-primary" onClick={props.handleCardClick} id={props.id} clickCount={props.clickCount}>Click this card</a> */}
                     </div>
                 </div>
-            </a>
+            </div>
+            // </a>
         );
     }
 }
