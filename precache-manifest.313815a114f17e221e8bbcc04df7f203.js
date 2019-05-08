d972fb9bbc6acf0902e6724c5caa413dbb64c1cc
Ali Thompson <alithom123@gmail.com>
2019-05-08 16:39:33 -0500

    Updates

--- precache-manifest.313815a114f17e221e8bbcc04df7f203.js
+++ precache-manifest.313815a114f17e221e8bbcc04df7f203.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "831a1640db745a1e4c05dc5b845dde59",
+    "revision": "31df16dd1730163cf4a4ae897609565a",
     "url": "/clicky-game/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clicky-game/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "06b4d548d47a0c8b6da5",
+    "revision": "f5bbf8a95a8d08d238c6",
     "url": "/clicky-game/static/css/main.a65a4919.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clicky-game/static/js/2.61b6dae8.chunk.js"
   },
   {
-    "revision": "06b4d548d47a0c8b6da5",
-    "url": "/clicky-game/static/js/main.e38ba1c1.chunk.js"
+    "revision": "f5bbf8a95a8d08d238c6",
+    "url": "/clicky-game/static/js/main.0e00f73a.chunk.js"
   },
   {
     "revision": "265538ab0e4606d350e6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clicky-game/precache-manifest.69460ba7f18c3a2ee72147f13f512c1b.js"
+  "/clicky-game/precache-manifest.313815a114f17e221e8bbcc04df7f203.js"
 );
 
 self.addEventListener('message', (event) => {
